c9a6cf257d009f5167843620e719bce8d2867739
Code Sandbox <hello@codesandbox.io>
2023-01-24 10:34:56 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.8eed62f42a7685ec5a7199f9ec2070d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17852c1b8322259e818573e0fb4427f1",
+    "url": "https://boagale.github.io/csb-zws2fn/index.html"
+  },
+  {
+    "revision": "5c36da5fbc9db296b55f",
+    "url": "https://boagale.github.io/csb-zws2fn/static/js/2.30c1e4c9.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "https://boagale.github.io/csb-zws2fn/static/js/2.30c1e4c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81a67406803b757c087d",
+    "url": "https://boagale.github.io/csb-zws2fn/static/js/main.bbedb53e.chunk.js"
+  },
+  {
+    "revision": "98e0b7ba5ea76d9b20f1",
+    "url": "https://boagale.github.io/csb-zws2fn/static/js/runtime-main.257c4906.js"
+  }
+]);
